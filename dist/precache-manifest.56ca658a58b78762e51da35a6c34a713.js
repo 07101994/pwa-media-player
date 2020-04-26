2aeda6a934e25c91a770e61266d0fb3842ebf0b2
Tushar Koshti <tushar.koshti@sequr.io>
2020-04-26 18:43:08 +0530

    Added manifest

---
+++ dist/precache-manifest.56ca658a58b78762e51da35a6c34a713.js
@@ -0,0 +1,9 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e140f3d7fe4f56911c1bc626f73eb78",
+    "url": "index.html"
+  },
+  {
+    "url": "main.ef6cb27109d68ed35741.js"
+  }
+]);
---
+++ dist/sw.js
@@ -0,0 +1,4 @@
+importScripts("precache-manifest.46d25ba17e4a08b28885bc1cce5b0200.js", "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
